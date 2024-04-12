ef96abf093bb5e005df9999b9bf411f91dd5cdb7
manogonas <enotnokot@gmail.com>
2024-04-13 00:57:32 +0400

    hell yeah

--- Lab_6_1.cpp
+++ Lab_6_1.cpp
@@ -1,30 +1,56 @@
 ﻿#include <iostream>
+#include <cmath>
+const double PI = acos(-1.0);
 
-class PointDecart {
-    PointDecart(double a, double r_): alpha(a), r(r_) {};
-    double get_angle() { return alpha; };
+class PointDecart { //Адаптировать к нему
+public:
+    PointDecart() {};
+    virtual double get_x() { return 0; };
+    virtual double get_y() { return 0; };
+    virtual void set_x(double x_) {};
+    virtual void set_y(double y_) {};
+    virtual void out() {};
+};
+
+class PointPolar { //Адаптируемый класс
+public:
+    PointPolar(double f, double r_): fi(f), r(r_) {};
+    double get_angle() { return fi; };
     double get_radius() { return r; };
-    void set_angle(double a) { alpha = a; };
+    void set_angle(double f) { fi = f; };
     void set_radius(double r_) { r = r_; };
-    void out() { std::cout << "Полярные координаты. Радиус = " << r << ", угол = " << alpha; }
+    void own_out() { std::cout << "Polar coordinates. Radius = " << r << ", angle = " << fi << std::endl; }
 private:
-    double alpha;
+    double fi;
     double r;
 };
 
-class PointPolar {
-    PointPolar(double x_, double y_): x(x_), y(y_) {};
-    double get_x() { return x; };
-    double get_y() { return y; };
-    void set_x(double x_) { x = x_; };
-    void set_y(double y_) { y = y_; };
-    void out() { std::cout << "Декартовы координаты. X = " << x << ", Y = " << y; }
+class ObjectPointPolarAdapter : public PointDecart {//Адаптер
+public:
+    ObjectPointPolarAdapter(PointPolar* adaptee_) { this->adaptee = adaptee_; };
+    double get_x() { return (adaptee->get_radius() * (cos(adaptee->get_angle() * PI / 180))); };
+    double get_y() { return (adaptee->get_radius() * (sin(adaptee->get_angle() * PI / 180))); };
+    void set_x(double x_) {
+        double y_ = ObjectPointPolarAdapter(adaptee).get_y();
+        adaptee->set_angle(atan(y_ / x_)); adaptee->set_radius(sqrt((y_ * y_) + (x_ * x_)));
+    };
+    void set_y(double y_) {
+        double x_ = ObjectPointPolarAdapter(adaptee).get_x();
+        adaptee->set_angle(atan(y_ / x_)); adaptee->set_radius(sqrt((y_ * y_) + (x_ * x_)));
+    };
+    void out() {
+        double x_ = ObjectPointPolarAdapter(adaptee).get_x();
+        double y_ = ObjectPointPolarAdapter(adaptee).get_y();
+        std::cout << "Cartesian coordinates. X = " << x_ << ", Y = " << y_ << std::endl;
+    };
 private:
-    double x;
-    double y;
+    PointPolar* adaptee;
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PointPolar* polar = new PointPolar(90, 5);
+    PointDecart* adapted = new ObjectPointPolarAdapter(polar);
+    polar->own_out();
+    adapted->out();
 }
